9bb62ebbe549863f5a551602c3e6bb8806d1468e
tschernysh <tschernysh@gmail.com>
2022-02-13 23:34:21 +0200

    click

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 let app = document.getElementsByClassName('content')[0]
 
+document.onmouseup = (e) => {
+    createHeart(e.clientX, e.clientY)
+    setTimeout(() => {
+        app.removeChild(app.children[0])
+    }, 1000)
+}
 
-
-const createHeart = () => {
+const createHeart = (x, y) => {
     let heartBlock = document.createElement('div')
     let leftPiece = document.createElement('div')
     let rightPiece = document.createElement('div')
@@ -12,16 +17,22 @@ const createHeart = () => {
     heartBlock.className = 'heart__block'
     leftPiece.className = 'left__piece'
     rightPiece.className = 'right__piece'
-    heartBlock.style.right = Math.random() * 100 + '%'
-    heartBlock.style.bottom = Math.random() * 50 + '%'
-    heartBlock.style.filter = `hue-rotate(${Math.random()*360}deg)`
+    if(x && y){
+        heartBlock.style.left = x + 'px'
+        heartBlock.style.top = y + 'px'    
+        heartBlock.classList.add('heart__click')
+    }else {
+        heartBlock.style.right = Math.random() * 100 + '%'
+        heartBlock.style.bottom = Math.random() * 50 + '%'
+        heartBlock.classList.add('heart__fly')
+    }
     heartBlock.style.transform = `scale(${scale}, ${scale})`
+    heartBlock.style.filter = `hue-rotate(${Math.random()*360}deg)`
     app.append(heartBlock)
     heartBlock.append(heart)
     heart.append(leftPiece)
     heart.append(rightPiece)
     app.append(heartBlock)
-    console.log(app.children);
 }
 
 setInterval(() => {
@@ -29,6 +40,6 @@ setInterval(() => {
     setTimeout(() => {
         app.removeChild(app.children[0])
     }, 3000)
-}, Math.random() * 200)
+}, 100)
 
 // document.onload = heartRainfall()
