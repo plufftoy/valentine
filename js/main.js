9c6811739e6a6597860778244af7532d3c122a09
tschernysh <tschernysh@gmail.com>
2022-02-13 23:51:39 +0200

    better click hearts

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-let app = document.getElementsByClassName('content')[0]
+let heartsField = document.getElementsByClassName('hearts')[0]
 
 document.onmouseup = (e) => {
     createHeart(e.clientX, e.clientY)
     setTimeout(() => {
-        app.removeChild(app.children[0])
+        heartsField.removeChild(heartsField.children[0])
     }, 1000)
 }
 
@@ -17,29 +17,42 @@ const createHeart = (x, y) => {
     heartBlock.className = 'heart__block'
     leftPiece.className = 'left__piece'
     rightPiece.className = 'right__piece'
+    heartBlock.style.filter = `hue-rotate(${Math.random()*360}deg)`
+    heartsField.append(heartBlock)
+    heartBlock.append(heart)
+    heart.append(leftPiece)
+    heart.append(rightPiece)
+    heartsField.append(heartBlock)
     if(x && y){
-        heartBlock.style.left = x + 'px'
-        heartBlock.style.top = y + 'px'    
+        heartBlock.style.transform = `scale(${scale}, ${scale})`
+        heartBlock.style.left = x - (heartBlock.scrollWidth / 2) + 'px'
+        heartBlock.style.top = y - (heartBlock.scrollHeight / 2) + 'px'    
         heartBlock.classList.add('heart__click')
     }else {
         heartBlock.style.right = Math.random() * 100 + '%'
         heartBlock.style.bottom = Math.random() * 50 + '%'
+        heartBlock.style.transform = `scale(${scale}, ${scale})`
         heartBlock.classList.add('heart__fly')
     }
-    heartBlock.style.transform = `scale(${scale}, ${scale})`
-    heartBlock.style.filter = `hue-rotate(${Math.random()*360}deg)`
-    app.append(heartBlock)
-    heartBlock.append(heart)
-    heart.append(leftPiece)
-    heart.append(rightPiece)
-    app.append(heartBlock)
 }
 
 setInterval(() => {
     createHeart()
     setTimeout(() => {
-        app.removeChild(app.children[0])
+        heartsField.removeChild(heartsField.children[0])
     }, 3000)
 }, 100)
 
-// document.onload = heartRainfall()
+let phase = 'start'
+
+const setPhase = (phase) => {
+    phase = phase
+    switch (phase) {
+        case 'start':
+            
+            break;
+    
+        default:
+            break;
+    }
+}
